0421bb8c2f2a7b4a7e9c6e01045a9a5cc568d129
mknayyy <michael.pulham@students.plymouth.ac.uk>
2021-01-01 15:33:14 +0000

    library, and seperate tasksk uploaded for people to work on + a read_me file

---
+++ Michael's Program/main.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+
+        
+
+    }
+}
+
---
+++ READ_ME.txt
@@ -0,0 +1,5 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+
+Put this at the top of your code, just underneath #include "mbed.h", to include the module support board :))
