cbf29329f6da5de7d4e2e5ac8b62f3e6da27bae9
mknayyy <michael.pulham@students.plymouth.ac.uk>
2021-01-01 20:14:06 +0000

    FIxes!, code should now build correctly

---
+++ mbed-os-empty test/main.cpp
@@ -0,0 +1,37 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+#define WAIT_TIME_MS 500 
+
+DigitalOut red(TRAF_RED1_PIN,1);         //Note the initial state
+DigitalOut amber(TRAF_YEL1_PIN,0);
+DigitalOut green(TRAF_GRN1_PIN,0);
+
+LCD_16X2_DISPLAY lcd;
+
+int main()
+{
+    lcd.puts("RED");
+    wait_us(1000000);
+
+    amber = 1;
+    lcd.cls();
+    lcd.puts("Amber");
+    wait_us(1000000);
+
+    green = 1;
+    lcd.cls();
+    lcd.puts("Green");    
+    wait_us(1000000);
+
+    lcd.cls();
+    lcd.puts("TASK-104");
+
+    while (true)
+    {
+        red = !red;
+        amber = !amber;
+        green = !green;
+        wait_us(WAIT_TIME_MS * 1000);
+    }
+}
